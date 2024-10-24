61e2eaf29589822f9955381f3c658b6c451c8efe
housamaj <housamaj@dukes.jmu.edu>
2024-10-24 11:11:36 -0400

    10/24/24

--- projects/covid-data/more_test_code.py
+++ projects/covid-data/more_test_code.py
@@ -1,12 +1,13 @@
 import sys
-import pandas
 import pandas as pd
+from fontTools.misc.textTools import caselessSort
+from numpy.ma.core import argmin, argmax
 
 data = pd.read_csv('us-counties.csv')
 
-rockingham_data = data[(data['fips'] == 51165.0) & (data['cases'] > 0)]
+rockingham_data = data[(data['fips'] == 51165.0) & (data['cases'] > 0)] # checking to see if cases > 0 is redundant
 
-first_rockingham_case = rockingham_data.iloc[0, 0]
+first_rockingham_case = rockingham_data.iloc[0, 0] # select the value from the 0th row and 0th column
 
 print("The first positive case in Rockingham occurred on: " + str(first_rockingham_case))
 
@@ -16,15 +17,46 @@ first_harrisonburg_case = harrisonburg_data.iloc[0, 0]
 
 print("The first positive case in Harrisonburg city occurred on: " + str(first_harrisonburg_case))
 
-rockingham_cases_array = rockingham_data['cases'].to_numpy()
+rockingham_data_array = rockingham_data.to_numpy()
 
-np_array_cases = rockingham_data['cases'].to_numpy()
+rockingham_list = list() # create list to store
+for i in range(1,len(rockingham_data_array)):
+    current_max = rockingham_data_array[i,4] - rockingham_data_array[i-1,4]
+    rockingham_list.append(current_max)
 
-for i in range(1,len(np_array_cases)):
-    case_value = np_array_cases[i] - np_array_cases[i-1]
-    if (np_array_cases[i+1] - np_array_cases[i]) > case_value:
-        case_value = np_array_cases[i+1] - np_array_cases[i]
-    else:
-        continue
+rockingham_index = argmax(rockingham_list)
+worst_rockingham_cases_date = rockingham_data_array[rockingham_index,0]
+print("The worst day for new reported cases in Rockingham county is "+ worst_rockingham_cases_date)
 
-print(case_value)
+harrisonburg_data_array = harrisonburg_data.to_numpy()
+
+harrisonburg_list = list()
+for i in range(1,len(harrisonburg_data_array)):
+    current_max = harrisonburg_data_array[i,4] - harrisonburg_data_array[i-1,4]
+    harrisonburg_list.append(current_max)
+
+harrisonburg_index = argmax(harrisonburg_list)
+worst_harrisonburg_cases_date = harrisonburg_data_array[harrisonburg_index,0]
+print("The worst day for new reported cases in Harrisonburg city is " + worst_harrisonburg_cases_date)
+
+rockingham_seven_day_list = list()
+for i in range(6,len(rockingham_data_array)):
+    seven_day_cumulative_r = rockingham_data_array[i,4] + rockingham_data_array[i-1,4] + rockingham_data_array[i-2,4] + rockingham_data_array[i-3,4] + rockingham_data_array[i-4,4] + rockingham_data_array[i-5,4] + rockingham_data_array[i-6,4]
+    rockingham_seven_day_list.append(seven_day_cumulative_r)
+
+seven_day_index_r = argmax(rockingham_seven_day_list)
+seven_day_date_1_r = rockingham_data_array[seven_day_index_r - 6,0]
+seven_day_date_2_r = rockingham_data_array[seven_day_index_r,0]
+print("The worst seven day period in Rockingham county was from " + seven_day_date_1_r + " to " + seven_day_date_2_r)
+
+harrisonburg_seven_day_list = list()
+for i in range(6,len(harrisonburg_data_array)):
+    seven_day_cumulative_h = harrisonburg_data_array[i,4] + harrisonburg_data_array[i-1,4] + harrisonburg_data_array[i-2,4] + harrisonburg_data_array[i-3,4] + harrisonburg_data_array[i-4,4] + harrisonburg_data_array[i-5,4] + harrisonburg_data_array[i-6,4]
+    harrisonburg_seven_day_list.append(seven_day_cumulative_h)
+
+seven_day_index_h = argmax(harrisonburg_seven_day_list)
+seven_day_date_1_h = harrisonburg_data_array[seven_day_index_h - 6,0]
+seven_day_date_2_h = harrisonburg_data_array[seven_day_index_h,0]
+print("The worst seven day period in Harrisonburg city was from " + seven_day_date_1_h + " to " + seven_day_date_2_h)
+
+
--- projects/covid-data/student_template.py
+++ projects/covid-data/student_template.py
@@ -1,62 +1,6 @@
 import sys
-import pandas
-
-def parse_nyt_data(file_path=''):
-    """
-    Parse the NYT covid database and return a list of tuples. Each tuple describes one entry in the source data set.
-    Date: the day on which the record was taken in YYYY-MM-DD format
-    County: the county name within the State
-    State: the US state for the entry
-    Cases: the cumulative number of COVID-19 cases reported in that locality
-    Deaths: the cumulative number of COVID-19 death in the locality
-
-    :param file_path: Path to data file
-    :return: A List of tuples containing (date, county, state, cases, deaths) information
-    """
-    # data point list
-    data=[]
-
-    # open the NYT file path
-    try:
-        fin = open(file_path)
-    except FileNotFoundError:
-        print('File ', file_path, ' not found. Exiting!')
-        sys.exit(-1)
-
-    # get rid of the headers
-    fin.readline()
-
-    # while not done parsing file
-    done = False
-
-    # loop and read file
-    while not done:
-        line = fin.readline()
-
-        if line == '':
-            done = True
-            continue
-
-        # format is date,county,state,fips,cases,deaths
-        (date,county, state, fips, cases, deaths) = line.rstrip().split(",")
-
-        # clean up the data to remove empty entries
-        if cases=='':
-            cases=0
-        if deaths=='':
-            deaths=0
-
-        # convert elements into ints
-        try:
-            entry = (date,county,state, int(cases), int(deaths))
-        except ValueError:
-            print('Invalid parse of ', entry)
-
-        # place entries as tuple into list
-        data.append(entry)
-
-
-    return data
+import pandas as pd
+import numpy as np
 
 def first_question(data):
     """
@@ -65,8 +9,15 @@ def first_question(data):
     # When was the first positive COVID case in Harrisonburg?
     :return:
     """
+    data = pd.read_csv('us-counties.csv') # function will read a csv file and import it as a dataframe
 
-    return
+    rockingham_data = data[(data['fips'] == 51165.0) & (data['cases'] > 0)] # basic pandas manipulation to extract rockingham data
+    first_rockingham_case = rockingham_data.iloc[0, 0] # extract the value from the 0th column and 0th row
+
+    harrisonburg_data = data[(data['county'] == 'Harrisonburg city') & (data['cases'] > 0)] # same logic as rockingham
+    first_harrisonburg_case = harrisonburg_data.iloc[0, 0]
+
+    return print("The first positive case in Rockingham county was " + first_rockingham_case + " and the first case in Harrisonburg city was " + first_harrisonburg_case)
 
 def second_question(data):
     """
@@ -76,37 +27,77 @@ def second_question(data):
     :return:
     """
 
-    # your code here
-    return
+    data = pd.read_csv('us-counties.csv')
+    rockingham_data = data[(data['fips'] == 51165.0) & (data['cases'] > 0)]
+    harrisonburg_data = data[(data['county'] == 'Harrisonburg city') & (data['cases'] > 0)]
+
+    rockingham_data_array = rockingham_data.to_numpy() # basic function for converting a dataframe to an array
+    harrisonburg_data_array = harrisonburg_data.to_numpy() # same as above
+
+    rockingham_list = list()  # create list to store all the new covid cases instead of cumulative
+    for i in range(1, len(rockingham_data_array)):
+        current_max = rockingham_data_array[i, 4] - rockingham_data_array[i - 1, 4]
+        rockingham_list.append(current_max)
+
+    rockingham_index = np.argmax(rockingham_list) # use argmax function to extract the index of the max
+    worst_rockingham_cases_date = rockingham_data_array[rockingham_index, 0] # access the date from that same index value in the array
+
+    harrisonburg_list = list() # same as above
+    for i in range(1, len(harrisonburg_data_array)):
+        current_max = harrisonburg_data_array[i, 4] - harrisonburg_data_array[i - 1, 4]
+        harrisonburg_list.append(current_max)
+
+    harrisonburg_index = np.argmax(harrisonburg_list)
+    worst_harrisonburg_cases_date = harrisonburg_data_array[harrisonburg_index, 0]
+
+    return print("The worst day for covid cases in Rockingham county was " + worst_rockingham_cases_date + " and the worst day for covid cases in Harrisonburg city was " + worst_harrisonburg_cases_date)
 
 def third_question(data):
     # Write code to address the following question:Use print() to display your responses.
     # What was the worst 7-day period in either the city and county for new COVID cases?
     # This is the 7-day period where the number of new cases was maximal.
 
-    return
+    data = pd.read_csv('us-counties.csv')
+    rockingham_data = data[(data['fips'] == 51165.0) & (data['cases'] > 0)]
+    harrisonburg_data = data[(data['county'] == 'Harrisonburg city') & (data['cases'] > 0)]
 
-if __name__ == "__main__":
-    data = parse_nyt_data('us-counties.csv')
+    rockingham_data_array = rockingham_data.to_numpy()
+    harrisonburg_data_array = harrisonburg_data.to_numpy()
 
-    for (date, county, state, cases, deaths) in data:
-        print('On ', date, ' in ', county, ' ', state, ' there were ', cases, ' cases and ', deaths, ' deaths')
+    rockingham_new_cases = list() # same as second question
+    for i in range(1, len(rockingham_data_array)):
+        current_max = rockingham_data_array[i, 4] - rockingham_data_array[i - 1, 4]
+        rockingham_new_cases.append(current_max)
 
+    rockingham_cumulative = list() # create list to store the seven-day sum of new cases
+    for i in range(6,len(rockingham_new_cases)):
+        seven_day_cumulative = rockingham_new_cases[i] + rockingham_new_cases[i-1] + rockingham_new_cases[i-2] + rockingham_new_cases[i-3] + rockingham_new_cases[i-4] + rockingham_new_cases[i-5] + rockingham_new_cases[i-6]
+        rockingham_cumulative.append(seven_day_cumulative)
 
-    # write code to address the following question: Use print() to display your responses.
-    # When was the first positive COVID case in Rockingham County?
-    # When was the first positive COVID case in Harrisonburg?
-    first_question(data)
+    rockingham_index = np.argmax(rockingham_cumulative)
+    rockingham_first_day = rockingham_data_array[rockingham_index,0] # access the array for the first day of the period
+    rockingham_second_day = rockingham_data_array[rockingham_index + 6,0] # access the array for the last day of the period
 
+    harrisonburg_new_cases = list() # same as before
+    for i in range(1,len(harrisonburg_data_array)):
+        current_max = harrisonburg_data_array[i,4] - harrisonburg_data_array[i-1,4]
+        harrisonburg_new_cases.append(current_max)
 
-    # write code to address the following question: Use print() to display your responses.
-    # What day was the greatest number of new daily cases recorded in Harrisonburg?
-    # What day was the greatest number of new daily cases recorded in Rockingham County?
-    second_question(data)
+    harrisonburg_cumulative = list() # same as before
+    for i in range(6,len(harrisonburg_new_cases)):
+        seven_day_cumulative = harrisonburg_new_cases[i] + harrisonburg_new_cases[i-1] + harrisonburg_new_cases[i-2] + harrisonburg_new_cases[i-3] + harrisonburg_new_cases[i-4] + harrisonburg_new_cases[i-5] + harrisonburg_new_cases[i-6]
+        harrisonburg_cumulative.append(seven_day_cumulative)
 
-    # write code to address the following question:Use print() to display your responses.
-    # What was the worst seven day period in either the city and county for new COVID cases?
-    # This is the 7-day period where the number of new cases was maximal.
-    third_question(data)
+    harrisonburg_index = np.argmax(harrisonburg_cumulative) # same as before
+    harrisonburg_first_day = harrisonburg_data_array[harrisonburg_index,0]
+    harrisonburg_second_day = harrisonburg_data_array[harrisonburg_index+6, 0]
+
+    return print("The worst seven day period in Rockingham county was from " + rockingham_first_day + " to " + rockingham_second_day + " and the worst seven day period in Harrisonburg city was from " + harrisonburg_first_day + " to " + harrisonburg_second_day)
+
+data = pd.read_csv('us-counties.csv')
+
+print(first_question(data))
+print(second_question(data))
+print(third_question(data))
 
 
